8aad467bd426a6e2ad1119eab1d9480e21d0b311
ALHASSANE DIALLO <alhassane.diallo@monboreal.ca>
2024-11-20 14:15:32 -0500

    Creation du dossier Models et ajout de la classe etudiant

---
+++ MauiApp1/Models/Etudiant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class Etudiant
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Email { get; set; }
+
+        public Etudiant(string name, string prenom, DateTime dateNaissance, string email)
+        {
+            Nom = name;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+            Email = email;
+        }
+    }
+}
